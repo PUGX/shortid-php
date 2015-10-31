720f44b252db35e749c5256a6ddf389a9d9fd1d1
Emanuele Minotto <minottoemanuele@gmail.com>
2015-10-31 14:06:41 +0100

    added tests

---
+++ tests/ShortidTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace PUGX\Shortid\Test;
+
+use PHPUnit_Framework_Assert;
+use PHPUnit_Framework_TestCase;
+use PUGX\Shortid\Shortid;
+
+class ShortidTest extends PHPUnit_Framework_TestCase
+{
+    public function testGenerate()
+    {
+        $generatedString = Shortid::generate();
+
+        $this->assertRegExp('/^[a-z0-9\_\-]{7,7}$/i', $generatedString);
+    }
+
+    public function testGetFactory()
+    {
+        $factory = Shortid::getFactory();
+
+        $this->assertInstanceOf('PUGX\Shortid\Factory', $factory);
+    }
+
+    public function testSetFactory()
+    {
+    	$factoryMock = $this->getMock('PUGX\Shortid\Factory');
+        Shortid::setFactory($factoryMock);
+
+        $this->assertSame($factoryMock, Shortid::getFactory());
+    }
+}
