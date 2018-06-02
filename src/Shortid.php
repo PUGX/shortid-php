b02616c548c6528b26d1b45c6ddec91ed5befe71
Egor Gruzdev <egorgruzdev@gmail.com>
2018-06-02 10:28:42 +0200

    JsonSerializable

--- src/Shortid.php
+++ src/Shortid.php
@@ -2,7 +2,7 @@
 
 namespace PUGX\Shortid;
 
-class Shortid
+class Shortid implements \JsonSerializable
 {
     /**
      * @var string
@@ -14,26 +14,46 @@ class Shortid
      */
     private static $factory;
 
+    /**
+     * @param string $id
+     */
     public function __construct(string $id)
     {
         $this->id = $id;
     }
 
+    /**
+     * @return string
+     */
     public function __toString(): string
     {
         return $this->id;
     }
 
-    public static function generate(int $length = null, string $alphabet = null, bool $readable = false): self
+    public function jsonSerialize()
+    {
+        return (string)$this;
+    }
+
+    /**
+     * @param int|null    $length
+     * @param string|null $alphabet
+     *
+     * @return self
+     */
+    public static function generate(int $length = null, string $alphabet = null): self
     {
         if (null === $length) {
             self::getFactory()->checkLength($length);
         }
         self::getFactory()->checkAlphabet($alphabet);
 
-        return self::getFactory()->generate($length, $alphabet, $readable);
+        return self::getFactory()->generate($length, $alphabet);
     }
 
+    /**
+     * @return Factory
+     */
     public static function getFactory(): Factory
     {
         if (null === self::$factory) {
@@ -43,11 +63,21 @@ class Shortid
         return self::$factory;
     }
 
+    /**
+     * @param Factory
+     */
     public static function setFactory(Factory $factory = null)
     {
         self::$factory = $factory;
     }
 
+    /**
+     * @param string $value
+     * @param int    $length
+     * @param string $alphabet
+     *
+     * @return bool
+     */
     public static function isValid(string $value, int $length = null, string $alphabet = null): bool
     {
         $length = $length ?? self::getFactory()->getLength();
