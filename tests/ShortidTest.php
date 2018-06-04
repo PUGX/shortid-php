d12adc70236cfa1b89be3a82abf916d6c118ed5b
Egor Gruzdev <egor.gruzdev@gmail.com>
2018-06-04 11:04:08 +0200

    [+] test3

--- tests/ShortidTest.php
+++ tests/ShortidTest.php
@@ -79,6 +79,6 @@ class ShortidTest extends TestCase
 	{
 		$generated = Shortid::generate();
 
-		$this->assertSame('"'.(string) $generated.'"', json_encode($generated));
+		$this->assertSame('"'.$generated.'"', json_encode($generated));
 	}
 }
